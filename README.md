f2bba515a97d0e5c7d97a8c2273f6604a8f7ee1e
YOomDev <ype.oomes@gmail.com>
2023-05-07 16:38:48 +0200

    Added README, added simple TTS and basic console implementation

---
+++ README.md
@@ -0,0 +1,19 @@
+# Streamer companion
+## About this project
+### What is this?
+This project is an attempt at creating a companion for streamers that play games or talk about certain topics.
+The bot itself is based on GPT4All which by itself doesn't know anything, in this project I will be attempting to strap a knowledge base that can be extended for any topic to the AI and have it provide info about the questions to the AI so that the AI can use it to summarize or word it as good as possible to answer the given question.
+
+### Inspiration
+The project was inspired by the streamer bots that could play games or answer chat messages.
+
+## Status
+### Planned parts
+ - (✔️) Console
+ - (✔️) GPT4All integration
+ - (❌) Easily extendable knowledge database system
+ - (✔️) TTS
+ - (❌) Voice recognition
+
+### Implementation status
+Current parts still have a lot of work to be done before actual completion of the project, current implementations are subject to change if the need for it arises.
--- bot.js
+++ bot.js
@@ -1,21 +1,36 @@
-// Main call
-start().catch(console.error); // Start the application
+// Project info:
+/* .
+ * .
+ */
 
-// Settings:
+//////////////
+// Settings //
+//////////////
+
 const filter = false;
 
-// Storage
+////////////////////////////////
+// Memory for running the bot //
+////////////////////////////////
+
 // Console
 const express = require('express');
 const app = express();
 
 // GPT4All
 // Note that the gpt4all we are using has problems by default on windows, instructions for fix below:
-// line 65: make sure there is a .exe extension like this -> "/.nomic/gpt4all.exe", this is only needed for windows users
+// line 65: make sure there is a .exe extension like this -> "/.nomic/gpt4all.exe", this is only needed for Windows users
 const GPT = require('gpt4all').GPT4All;
 const chat = new GPT('gpt4all-lora-unfiltered-quantized', true); // Default is 'gpt4all-lora-quantized' model
 
-// queue's and busy booleans for all different parts
+// Text To Speech
+const say = require('say');
+let voicesList = [];
+say.speak("Initializing", null, 2.0, (err) => { if (err) { return console.error(err) } });
+function getVoices() { return new Promise((resolve) => { say.getInstalledVoices((err, voice) => { if (err) { console.error(err); } return resolve(voice); }) }) }
+async function usingVoices() { voicesList = await getVoices(); console.log(voicesList) }
+
+// Queue's and busy booleans for all different parts
 let tasksBusy  = { speaking: false, thinking: false, listening: false, console: false };
 let tasksQueue = { speaking: []   , thinking: []   , listening: []   , console: []    };
 
@@ -25,7 +40,6 @@ let tasksQueue = { speaking: []   , thinking: []   , listening: []   , console: 
 
 // TODO  list:
 /* Setup console
- * Add TTS
  * Setup knowledge database system
  * Setup filters for output
  * Add voice recognition
@@ -37,24 +51,38 @@ async function init() {
    await chat.init(); // Initialize and download missing files
    console.log("Opening...");
    await chat.open(); // Open the connection with the model
+   console.log("Voices:");
+   await usingVoices();
    console.log("Initialized!");
 }
 
 function isBusy() { return tasksBusy.speaking || tasksBusy.thinking || tasksBusy.listening || tasksBusy.server; }
 
 async function start() {
-   await init();
+   await init(); // Make sure the modules are initialized correctly
+   while (isBusy()) {} // Loop until finished
 
-   // Loop until finished
-   while (isBusy()) {}
-
+   // Make sure the modules are closed correctly
    chat.close();
-
-   // TODO: implement loading all the parts
+   stopServer();
 }
 
 function parseCommand(cmd) {
-   // TODO: implement basic command parsing
+   console.log(cmd);
+   const params = cmd.substring(1,cmd.length).split("\/");
+   console.log(params);
+   if (equalsCaseSensitive(params[0], "cmd")) {
+      switch (params[1]) {
+         case "say": {
+
+         }
+         case "start":
+            start().catch((err) => { console.error(err); });
+            break;
+         default:
+            break;
+      }
+   }
 }
 
 ////////////////////
@@ -63,6 +91,22 @@ function parseCommand(cmd) {
 
 // tasksBusy.speaking = true;
 // TODO: implement
+
+function speak(message) {
+   tasksQueue.speaking.push(message);
+   if (!tasksBusy.speaking) { speakQueue(); }
+}
+
+function speakQueue() {
+   tasksBusy.speaking = true;
+   const msg = tasksQueue.speaking[0];
+   tasksQueue.speaking.splice(0,1);
+   say.speak(msg, null, 1.0, (err) => {
+      if (err) { tasksBusy.speaking = false; console.error(err); }
+      if (tasksQueue.speaking.length > 0) { speakQueue(); }
+      else { tasksBusy.speaking = false; }
+   });
+}
 
 /////////
 // GPT //
@@ -84,16 +128,26 @@ function parseCommand(cmd) {
 
 // TODO: implement
 
+// info:
+/* can load modules and keep the names and index pages in memory
+ * loads only the needed memory parts of the modules when requested and doesn't keep them in memory
+ * needs to be able to get knowledge from parent items, like when thinking of planks it needs to know things about logs and trees and such
+ */
+
 ///////////////////
 // Control panel //
 ///////////////////
 
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 
-app.get(/.*$/, (req, res) => {
+app.get("/cmd/*", (req, res) => {
+   res.redirect("/"); // redirects back to the home page
+   console.log(tasksBusy.console);
    parseCommand(req.url);
-   res.render("index"); // Renders the bots console to the user
 });
+
+app.get("/", (req, res) => { res.render("index"); tasksBusy.console = true; }); // Renders the bots console to the user
 
 app.listen(3000, () => { tasksBusy.server = true; });
 
@@ -104,3 +158,10 @@ function stopServer() { tasksBusy.server = false; }
 ///////////
 
 async function sleep(seconds) { return new Promise((resolve) => setTimeout(resolve, seconds * 1000)); }
+
+function equalsCaseSensitive(first, second) {
+   if (first.length !== second.length) { return false; }
+   console.log(first + "?" + second);
+   for (let i = 0; i < first.length; i++) { if (first[i] !== second[i]) { console.log(first[i] + "?" + second[i]); return false; } }
+   return true;
+}
